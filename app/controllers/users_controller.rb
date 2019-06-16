e12ce6b543b5ed6bdcc1fec1e29a2138d0f26d4b
non.mu <mnexpo5@yahoo.co.jp>
2019-06-16 04:16:31 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -23,7 +28,14 @@ class UsersController < ApplicationController
     end
   end
   
-　private
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+
+  
+ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
